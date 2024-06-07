d3fdf71655d8bcb4817c9dcb3a75887cc8a304f1
leeleeleeleejun <wnstjr6293@naver.com>
2024-06-07 12:41:57 +0900

    feat: 네이버 지도 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { NavermapsProvider } from "react-naver-maps";
 
 import {
@@ -12,7 +8,6 @@ import {
 } from "react-naver-maps";
 
 function MyMap() {
-  // instead of window.naver.maps
   const navermaps = useNavermaps();
 
   return (
@@ -26,12 +21,11 @@ function MyMap() {
 }
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const NAVER_MAP_API = import.meta.env.VITE_NAVER_MAP_API;
   return (
     <>
       <NavermapsProvider
-        ncpClientId="yvzzner2mh"
+        ncpClientId={NAVER_MAP_API}
         // or finClientId, govClientId
       >
         <MapDiv
@@ -43,26 +37,6 @@ function App() {
           <MyMap />
         </MapDiv>
         ;
-        {/* <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p> */}
       </NavermapsProvider>
     </>
   );
