7e7b21afc64fe9864afbcf4aa55906a764fce886
leeleeleeleejun <wnstjr6293@naver.com>
2024-06-07 19:10:32 +0900

    feat: 내위치로 이동 함수 구현 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
+import { useEffect, useState } from "react";
 import { NavermapsProvider } from "react-naver-maps";
-
 import {
   Container as MapDiv,
   NaverMap,
@@ -7,34 +7,96 @@ import {
   useNavermaps,
 } from "react-naver-maps";
 
-function MyMap() {
+function MyMap(onCurrent: boolean) {
   const navermaps = useNavermaps();
+  const infowindow = new naver.maps.InfoWindow({ content: "" });
+
+  const [location, setLocation] = useState({ latitude: 0, longitude: 0 });
+  const [map, setMap] = useState<naver.maps.Map | null>(null);
+
+  //현재 내위치 이동
+  useEffect(() => {
+    currentLocation(setLocation);
+    if (map) {
+      map.panTo(new navermaps.LatLng(location.latitude, location.longitude));
+      infowindow.setContent(
+        '<div style="padding:20px;">' +
+          "geolocation.getCurrentPosition() 위치" +
+          "</div>"
+      );
+
+      infowindow.open(
+        map,
+        new navermaps.LatLng(location.latitude, location.longitude)
+      );
+    }
+  }, [onCurrent]);
 
   return (
     <NaverMap
-      defaultCenter={new navermaps.LatLng(37.3595704, 127.105399)}
+      defaultCenter={new navermaps.LatLng(36.8547588, 127.1432806)}
       defaultZoom={15}
+      ref={setMap}
     >
-      <Marker defaultPosition={new navermaps.LatLng(37.3595704, 127.105399)} />
+      <Marker defaultPosition={new navermaps.LatLng(36.8307488, 127.1432806)} />
     </NaverMap>
   );
 }
 
+function currentLocation(
+  setLocation: React.Dispatch<
+    React.SetStateAction<{
+      latitude: number;
+      longitude: number;
+    }>
+  >
+) {
+  let location;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+
+  function success(position: GeolocationPosition) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    setLocation({ latitude, longitude });
+  }
+
+  function error() {
+    alert("Unable to retrieve your location.");
+  }
+  return location;
+}
+
 function App() {
   const naverMapApi = import.meta.env.VITE_NAVER_MAP_API;
-  console.log(naverMapApi);
+  const [onCurrent, setOnCurrent] = useState(false);
   return (
     <>
       <NavermapsProvider ncpClientId={naverMapApi}>
+        <button
+          style={{
+            width: "60px",
+            height: "50px",
+          }}
+          onClick={() => {
+            setOnCurrent(!onCurrent);
+          }}
+        >
+          내 위치
+        </button>
+
         <MapDiv
           style={{
             width: "100%",
-            height: "600px",
+            height: "1000px",
           }}
         >
-          <MyMap />
+          <MyMap onCurrent={onCurrent} />
         </MapDiv>
-        ;
       </NavermapsProvider>
     </>
   );
