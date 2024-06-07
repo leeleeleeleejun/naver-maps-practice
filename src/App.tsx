c3cf7a126905071e851af803cca242a41f0e8a53
leeleeleeleejun <wnstjr6293@naver.com>
2024-06-07 12:43:01 +0900

    Revert "feat: 네이버 지도 연결"
    This reverts commit d3fdf71655d8bcb4817c9dcb3a75887cc8a304f1.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 import { NavermapsProvider } from "react-naver-maps";
 
 import {
@@ -8,6 +12,7 @@ import {
 } from "react-naver-maps";
 
 function MyMap() {
+  // instead of window.naver.maps
   const navermaps = useNavermaps();
 
   return (
@@ -21,11 +26,12 @@ function MyMap() {
 }
 
 function App() {
-  const NAVER_MAP_API = import.meta.env.VITE_NAVER_MAP_API;
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <NavermapsProvider
-        ncpClientId={NAVER_MAP_API}
+        ncpClientId="yvzzner2mh"
         // or finClientId, govClientId
       >
         <MapDiv
@@ -37,6 +43,26 @@ function App() {
           <MyMap />
         </MapDiv>
         ;
+        {/* <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p> */}
       </NavermapsProvider>
     </>
   );
